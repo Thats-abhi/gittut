213a1bde22c7caa8daefa2e5f3cbf2d326db5f6e
abhishek <thats.abhi18@gmail.com>
2025-09-27 17:45:41 +0530

    sum and subtract functions are added.

---
+++ src/main/java/com/project/testproject/Calculator.java
@@ -0,0 +1,11 @@
+package com.project.testproject;
+
+public class Calculator {
+    Calculator(){};
+    public int sum(int a,int b){
+        return a+b;
+    }
+    public int sub(int a,int b){
+        return a+b;
+    }
+}
