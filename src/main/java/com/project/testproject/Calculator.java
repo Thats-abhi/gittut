eff3bfa3cd15c2e9a51f4807792b7885e5eb6b3d
abhishek <thats.abhi18@gmail.com>
2025-09-27 19:09:42 +0530

    modulus and gcd function is added

--- src/main/java/com/project/testproject/Calculator.java
+++ src/main/java/com/project/testproject/Calculator.java
@@ -1,5 +1,7 @@
 package com.project.testproject;
 
+import static java.util.Collections.swap;
+
 public class Calculator {
     Calculator(){};
     public int sum(int a,int b){
@@ -20,4 +22,20 @@ public class Calculator {
         }
         return res;
     }
+    public int gcd(int a,int b){
+        if(a==0) return b;
+        if(b==0) return a;
+        return gcd(a,b%a);//galat hai ye bhai.
+    }
+    public int modulus(int a,int b){
+        int res=0;
+        try{
+            res = a%b;
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return res;
+    }
 }
+
