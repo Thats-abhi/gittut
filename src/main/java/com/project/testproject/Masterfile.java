82592fdf61c1b0bf9b1a21431fcbfc55c1aedfa8
abhishek <thats.abhi18@gmail.com>
2025-11-16 00:10:11 +0530

    added new file for master and a method to modify and print message.

---
+++ src/main/java/com/project/testproject/Masterfile.java
@@ -0,0 +1,8 @@
+package com.project.testproject;
+
+public class Masterfile {
+    public String message;
+    public void modify(String messageFromMaster){
+        System.out.println(messageFromMaster);
+    }
+}
