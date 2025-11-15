ed14bc9d03c66c463c722e4c6515719bcd60fc79
abhishek <thats.abhi18@gmail.com>
2025-11-16 00:02:41 +0530

    added new file for gitmessage

---
+++ src/main/java/com/project/testproject/Gittest.java
@@ -0,0 +1,11 @@
+package com.project.testproject;
+
+public class Gittest {
+    public String message;
+    public Gittest(String message){
+        System.out.println(message);
+    }
+    public void modify(String messageFromGit){
+        System.out.println(messageFromGit);
+    }
+}
